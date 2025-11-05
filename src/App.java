d13bf68a81b14d808b1d818ccf6fbfeb3f5d3a2e
is13mael <ismaelparramoussaif@gmail.com>
2025-11-05 18:57:53 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,40 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.print("Por favor, introduzca un número entero positivo: ");
+        int numero = Integer.parseInt(System.console().readLine());
+
+        int aux = numero;
+        int divisor = 1;
+        int longitud = 0;
+
+        while (aux > 0) {
+            aux = aux / 10;
+            longitud++;
+        }
+
+        for(int i = 1; i < longitud; i++){
+            divisor = divisor * 10;
+        }
+
+        aux = numero;
+        String resultado = "";
+
+        while (divisor > 0) {
+            int digito = aux / divisor;
+            aux = aux % divisor;
+            divisor = divisor / 10;
+
+            int contador = 0;
+            while (contador < digito) {
+                resultado = resultado + "|";
+                contador++;
+            }
+
+            if (divisor > 0) {
+                resultado = resultado + " - ";
+            }
+        }
+
+        System.out.printf("El %d en decimal es el %s en el sistema de palotes", numero, resultado);
+    }
+}
